f554d868cd78ddb73aa19965a4a3adf7d4222a8a
yuki <xzj32ejt8qsmr9tmx9xf@docomo.ne.jp>
2021-02-23 11:25:53 +0900

    indexアクションの追加

---
+++ app/controllers/communities_controller.rb
@@ -0,0 +1,21 @@
+class CommunitiesController < ApplicationController
+
+  def index
+    @communities = Community.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
     :sessions => 'users/sessions'
-  } 
+  }
   root 'users#index'
 
 resources :users, :only => [:index, :show]
-
+resources :communities, :except => [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
