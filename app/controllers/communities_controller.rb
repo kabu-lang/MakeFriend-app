95631ed1e88aae0458b51b78ea9bec456e510684
yuki <xzj32ejt8qsmr9tmx9xf@docomo.ne.jp>
2021-02-27 09:17:23 +0900

    コミュニティの検索機能の実装

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,7 +1,15 @@
 class CommunitiesController < ApplicationController
+  before_action :set_community, only: [:show, :edit, :update]
+
 
   def index
-    @communities = Community.all
+    if !params[:send_category].nil? && params[:category_id][:id] != ""
+      @communities = Community.includes(:category).where(categories:{id:params[:category_id][:id]})
+    elsif !params[:send_prefecture].nil? && params[:prefecture_id][:id] != ""
+      @communities = Community.where(prefecture_id:params[:prefecture_id][:id])
+    else
+      @communities = Community.all
+    end
   end
 
   def new
@@ -10,6 +18,8 @@ class CommunitiesController < ApplicationController
 
   def create
     @community = Community.new(community_params)
+
+    @community.participation_people = 1
     @community.date = DateTime.new(
       params[:community]["date(1i)"].to_i,
       params[:community]["date(2i)"].to_i,
@@ -17,7 +27,9 @@ class CommunitiesController < ApplicationController
       params[:community]["date(4i)"].to_i,
       params[:community]["date(5i)"].to_i
     )
+
     if @community.save
+      @community.users << current_user
       redirect_to @community
     else
       render 'new'
@@ -26,20 +38,35 @@ class CommunitiesController < ApplicationController
 
 
   def show
-    @community = Community.find(params[:id])
   end
 
   def edit
   end
 
   def update
+    if @community.update(community_params)
+      @community.date = DateTime.new(
+        params[:community]["date(1i)"].to_i,
+        params[:community]["date(2i)"].to_i,
+        params[:community]["date(3i)"].to_i,
+        params[:community]["date(4i)"].to_i,
+        params[:community]["date(5i)"].to_i
+      )
+      redirect_to @community
+    else
+      render 'edit'
+    end
   end
 
 
   private
 
+  def set_community
+    @community = Community.find(params[:id])
+  end
+
   def community_params
-    params.require(:community).permit(:name, :prefecture_id, :municipality, :area_detail, :max_people, :category_id)
+    params.require(:community).permit(:name, :prefecture_id, :municipality, :area_detail, :max_people, :category_id, :author)
   end
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,15 +6,17 @@ class UsersController < ApplicationController
     if params[:send_category] == nil && params[:send_prefecture] == nil
       @users = User.all
     elsif !params[:send_category].nil?
-      @users = User.includes(:categories).where(categories:{id:params[:category_id][:id]} )
+      @users = User.includes(:categories).where(categories:{id:params[:category_id][:id]})
     else
       
-      @users = User.where(prefecture_id:params[:user][:prefecture_id][:id])
+      @users = User.where(prefecture_id:params[:prefecture_id][:id])
+
     end
   end
 
   def show
     @user = User.find(params[:id])
+
   end
 
 end
